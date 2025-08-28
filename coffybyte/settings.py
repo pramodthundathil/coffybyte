74874a54e32b65b1b241df5531d3546eeb109cca
pramod <gopinath.pramod@gmail.com>
2025-08-28 14:26:21 +0530

    cores allowed

--- coffybyte/settings.py
+++ coffybyte/settings.py
@@ -93,6 +93,29 @@ SWAGGER_SETTINGS = {
     },
 }
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+CORS_ALLOWED_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
 # CORS_ALLOWED_ORIGINS = [
    
 #     "*"
